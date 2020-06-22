7de1ecb1e9addad66a3708e7bd32bd7984038f1c
Fareeha Farhath <fareeha.farhath@tafensw.edu.au>
2020-06-23 07:40:27 +1000

    addedcomments

--- Day1 22-06-2020/index.js
+++ Day1 22-06-2020/index.js
@@ -1,27 +1,29 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 app.use(express.static('wwwroot'))
-const port=3000;
+const port = 3000;
 app.get('/about', function (req, res) {
     res.send('<h1>This is the about us page!</h1>');
-});
-// /print
+});//http://localhost:3000/about
+// /print http://localhost:3000/print?message=Hello
 // message=Hello
 app.get('/print', function (req, res) {
     let message = req.query.message;
     res.send('<h1>Your message was: ' + message + '</h1>');
 });
-// /numbers
-app.get('/numbers', function (req, res) {
-    let start = parseInt(req.query.start);
-    let stop = parseInt(req.query.stop);
-    let html = '<ul>';
-    for (let i = start; i < stop; i++) {
-        html += '<li>' + i + '</li>';
-    }
-    html += '</ul>';
-    res.send(html);
+// /numbers printing on webpage  http://localhost:3000/numbers?start=10&stop=15
+// / SAME CAN BE USED ,create numbers-form.html
+//In form action='/number' does same thing numbers printing on webpage using input from form  http://localhost:3000/numbers-form.html
+app.get('/numbers', function (req, res) { 
+let start = parseInt(req.query.start);
+let stop = parseInt(req.query.stop);
+let html = '<ul>';
+for (let i = start; i < stop; i++) {
+    html += '<li>' + i + '</li>';
+}
+html += '</ul>';
+res.send(html);
 });
 //app.listen should be always at the bottom
-app.listen(port,()=>console.log("For obtain data through forms and store intobrowser and display here at HTML"));
+app.listen(port, () => console.log("For obtain data through forms and store intobrowser and display here at HTML"));
 
